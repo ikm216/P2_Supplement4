9fb84622ac7bd5b0783d3eb848da1fa452f8d7d8
ikm216 <ikmclassof22@gmail.com>
2024-12-01 17:53:15 -0500

    test: added test for generating random hexidecimal characters

--- test_java.java
+++ test_java.java
@@ -1,3 +1,11 @@
+import org.junit.*;
+
 public class test_java {
-    
+    @Test
+    public void test_should_return_HexChars(){
+        String hex = su.hexChars();
+        Assert.assertNotNull(hex);
+        Assert.assertEquals(36, hex.length());
+        Assert.assertTrue(hex.matches("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"));
+    }
 }
