41870893938c807a9c941badfcf05ff8994cebbe
ikm216 <ikmclassof22@gmail.com>
2024-12-01 18:15:06 -0500

    feat: added feature for generating hexdecimal characters and a main to run the code to see the output

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String hex = su.hexChars();
+        System.out.println("Hex: " + hex);
+    }
+}
--- su.java
+++ su.java
@@ -1,3 +1,19 @@
+import java.security.*;
+
 public class su{
-    
+    public static String hexChars(){
+        SecureRandom rand = new SecureRandom();
+        byte[] bytes = new byte[16];
+        rand.nextBytes(bytes);
+
+        StringBuilder hex = new StringBuilder();
+        for(byte by: bytes){
+            hex.append(String.format("%02x", by));
+        }
+
+        return String.format("%s-%s-%s-%s-%s", hex.substring(0, 8), hex.substring(8, 12),
+        hex.substring(12, 16),
+        hex.substring(16, 20),
+        hex.substring(20, 32));
+    }
 }
