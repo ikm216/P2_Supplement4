b2b5d5ad2349cd3cedba3668df43ba1817268f87
ikm216 <ikmclassof22@gmail.com>
2024-12-01 19:02:54 -0500

    feat: added feature for inserting and holding string

--- Main.java
+++ Main.java
@@ -5,5 +5,10 @@ public class Main {
 
         String hash_string = su.hashString("chicken");
         System.out.println("Hash: " + hash_string);
+
+        arrString arr = new arrString(10);
+        arr.insert(hex, 15);
+        System.out.println("Inserted Value at Index 5: " + arr.getArr()[5]);
+
     }
 }
--- arrString.java
+++ arrString.java
@@ -1,5 +1,18 @@
 public class arrString{
-    
+    private String[] arr;
+
+    public arrString(int len){
+        this.arr = new String[len];
+    }
+
+    public void insert(String val, int i){
+        int index = i % arr.length;
+        arr[index] = val;
+    }
+
+    public String[] getArr(){
+        return arr;
+    }
 
     
 }
